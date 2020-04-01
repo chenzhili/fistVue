f298cedc505676fbb98833555b5cb117a40174fb
chenzl <chenzl@qianlong2.net>
2020-04-01 17:52:27 +0800

    improve proj

--- src/App.vue
+++ src/App.vue
@@ -54,6 +54,10 @@ export default {
 };
 </script>
 <style lang="scss">
+@import './styles/common.scss';
+@import "./styles/scrollBar.scss";
+@import "./styles/variable.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/utils/index.js
+++ src/utils/index.js
@@ -12,3 +12,39 @@ export function type(obj) {
         class2type[core_toString.call(obj)] || "object" :
         typeof obj;
 }
+
+/**
+ * 存储localStorage
+ */
+export const setStore = (name, content) => {
+  if (!name) return;
+  if (typeof content !== 'string') {
+    content = JSON.stringify(content);
+  }
+  window.localStorage.setItem(name, content);
+}
+
+/**
+ * 获取localStorage
+ */
+export const getStore = name => {
+  if (!name) return;
+  var value = window.localStorage.getItem(name);
+  if (value !== null) {
+    try {
+      value = JSON.parse(value);
+    } catch (e) {
+      // eslint-disable-next-line no-self-assign
+      value = value;
+    }
+  }
+  return value;
+}
+
+/**
+ * 删除localStorage
+ */
+export const removeStore = name => {
+  if (!name) return;
+  window.localStorage.removeItem(name);
+}
